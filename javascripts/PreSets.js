5d815e9c45fd78fe70ef24d0a94528c185be56e2
VisualizationLAB <D3Wannabe@users.noreply.github.com>
2013-09-30 12:00:35 -0400

    Update PreSets.js
    v4

--- javascripts/PreSets.js
+++ javascripts/PreSets.js
@@ -1,17 +1,38 @@
 var width = screen.availWidth * 0.8,
 	height = screen.availHeight * .2,
-   cellSize = width * 0.013; // cell size
+   //cellSize = width * 0.013; // cell size
+   cellSize = 13.5; // cell size
 
 var widthLine = screen.availWidth * .5 ,
 heightLine = height,
 //widthbar = screen.availWidth * 0.001953;
 widthbar = screen.availWidth * 0.001954;
-
-var heightC4 = cellSize * 14,
-widthC4 = cellSize * 60;
-
+var widthBarChart = cellSize * 11;
 var widthCalendar = cellSize * 58;
 var heightCalendar = cellSize * 10;
+
+console.log("widthBarChart : " + widthBarChart)
+
+var heightC4 = cellSize * 10; // imp
+var widthC4 = cellSize * 60; //imp
+
+switch(screen.availWidth)
+{
+	case 1366: // Laptop
+		var mainTableWidth = (3 * widthBarChart) + widthCalendar ;//imp
+		break;
+	case 1280: // Monitor
+		var mainTableWidth = (3 * widthBarChart) + widthCalendar ;//imp
+		break;
+	//case 1280: // Projector
+		//var mainTableWidth = (4 * widthBarChart) + widthCalendar ;//imp
+		//break;
+}	
+
+
+console.log("mainTableWidth : " + mainTableWidth)
+
+
 //var verticalCalendarOffset = heightCalendar * .1;
 if (screen.availWidth > 1280) { 
 //console.log("here");
@@ -25,9 +46,11 @@ else
 console.log(screen.availWidth);
 console.log(screen.availHeight);
 
-var horizontalCalendarOffset = widthCalendar * .1;
-var calendarStart = 2013;
-var calendarEnd = 2014;
+//var horizontalCalendarOffset = widthCalendar * .1;
+var horizontalCalendarOffset = 150;
+
+var calendarStart = 2012;
+var calendarEnd = 2015;
 
 var cellpadding = 2;
 var cellpaddingD = 0;
@@ -35,7 +58,23 @@ var cellpaddingD = 0;
 var borderT = 0;
 var borderT2 = 0;
 
-var widthBarChart = cellSize * 10;
+
+
+var CenterSectionHt = 340;
+var CenterSectionWidth = 900;
+var CalendarTransformX = 180;
+switch(screen.availWidth)
+{
+	case 1366: // Laptop
+		var CalendarTransformY = 40;
+		break;
+	case 1280: // Monitor
+		var CalendarTransformY = 90;
+		break;
+	case 1280: // Projector
+		var CalendarTransformY = 90;
+		break;
+}	
 
 var dataOriginal;
 var arrayRange = 12;
@@ -43,7 +82,9 @@ var domainMax = 20000000;
 var domainMin = 0;
 //Controls
 var select_1 = "err";
-var strSelectID = "FAILED RECORDS";
+var filterVar = "";
+var strFilter = "";
+var strSelectID = "FAILURES";
 var select_Class = "";
 var BIGDATA = [];
 var dataBIG = [];
@@ -61,6 +102,7 @@ var BarErrChartData = [];// Total data Error Types
 var BarErrChartDataSelect = [];// Daily data Error Types
 
 var selectedDate = ""
+var selectedYr = calendarStart;
 var selectedDateLast = ""
 var lastClicked;
 var lastSelectedLabel;
