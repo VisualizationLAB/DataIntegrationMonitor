8a4c97246f9f913706f8ba4ebd32dc300465ad07
VisualizationLAB <D3Wannabe@users.noreply.github.com>
2013-09-30 11:59:47 -0400

    Update BuildFileBarChart.js
    v4

--- javascripts/BuildFileBarChart.js
+++ javascripts/BuildFileBarChart.js
@@ -3,8 +3,8 @@
 function buildFileBarChart() {
 
 //console.log("BarChartData");
-
-console.log(BarChartData);
+//console.log("Replacing BarChartData");
+//console.log(BarChartData);
 Bar_filterCat = d3.nest() // didnt FIND this declared in global variables
 			.key(function(d) { return d.File_Layouts; })
 			.rollup(function(d) { 
@@ -40,18 +40,23 @@ BarErrChartData = Bar_Error;
 
 d3.selectAll(".SummDataDivT").remove();
 d3.selectAll(".SummDataDiv").remove();
+d3.selectAll(".SummDataDivC").remove();
 
 d3.select(".SummData")
+//d3.select(".summ_row4")
 		.append("table")
 		.attr("border", borderT)
 		.attr("class", "SummDataDivT")
 			.append("tr")
 				.append("td")
 					//.selectAll("svg")
-					.text("TOTAL")
+					.html(function() {
+						return "<span style='color:grey;font-size:11px;'>Total </span>" + "<strong style = 'color:steelblue;font-weight:bold;font-size:11px;'>"+document.getElementById(select_1).textContent+"</strong> <span style='color:red;font-size:11px;'>" + selectedYr + "</span>";
+						})
+					//.text("TOTAL")
 					//.append("svg")
 					//.append("text", "TOTALS")
-					.attr("style", "color:steelblue;font-weight:bold;font-size:12px;")
+					//.attr("style", "color:steelblue;font-weight:bold;font-size:12px;")
 					.attr("valign", "bottom")
 					.attr("align", "center");
 					
@@ -67,7 +72,9 @@ modularBarCharts (data,".SummDataDiv");
 
 
 d3.selectAll(".SummDataFileT").remove();
+//d3.selectAll(".SummDataFileTD").remove();
 d3.selectAll(".SummDataFile").remove();
+d3.selectAll(".SummDataFileC").remove();
 
 d3.select(".SummDataFileTD")
 		.append("table")
@@ -75,8 +82,10 @@ d3.select(".SummDataFileTD")
 		.attr("border", borderT)
 			.append("tr")
 				.append("td")
-					.text("TOTAL")
-					.attr("style", "color:steelblue;font-weight:bold;font-size:12px;")
+					.html(function() {
+						return "<span style='color:grey;font-size:11px;'>Total </span>" + "<strong style = 'color:steelblue;font-weight:bold;font-size:11px;'>"+document.getElementById(select_1).textContent+"</strong> <span style='color:red;font-size:11px;'>" + selectedYr + "</span>";
+						})
+					//.attr("style", "color:steelblue;font-weight:bold;font-size:12px;")
 					.attr("valign", "bottom")
 					.attr("align", "center");
 					
@@ -88,6 +97,8 @@ d3.select(".SummDataFileT")
 					.attr("align", "center");
 					
 var data = d3.entries(Bar_filterCat);
+//console.log("data")
+//console.log(data)
 modularBarCharts (data, ".SummDataFile" );
 /////////////////////////////////// ERROR BARS
 
@@ -102,7 +113,9 @@ d3.selectAll("svg")
 			
 if(select_1 == "err" || select_1 == "email" || select_1 == "fileT") {
 
-d3.selectAll(".dataFile_err")
+//d3.selectAll(".dataFile_err")
+d3.selectAll(".dataTableSumm")
+//d3.selectAll(".CalSumm")
 	.append("td")
     .attr("class", "errDataDivTD")
 	.attr("valign", "top")
@@ -116,8 +129,11 @@ d3.select(".errDataDivTD")
 		.attr("border", borderT)
 			.append("tr")
 				.append("td")
-					.text("TOTAL ERRORS")
-					.attr("style", "color:steelblue;font-weight:bold;font-size:12px;")
+					//.text("TOTAL ERRORS")
+					.html(function() {
+						return "<span style='color:grey;font-size:11px;'>Errors </span><strong style = 'color:steelblue;font-weight:bold;font-size:11px;'>" +document.getElementById(select_1).textContent+ " </strong><span style='color:red;font-size:11px;'>" + selectedYr + "</span>";
+					})
+					//.attr("style", "color:steelblue;font-weight:bold;font-size:12px;")
 					.attr("valign", "bottom")
 					.attr("align", "center");
 		
@@ -216,8 +232,11 @@ d3.select(".DailyDataFileTD")
 		.attr("border", borderT)
 			.append("tr")
 				.append("td")
-					.text(document.getElementById("currDate").textContent + ": File Layouts")
-					.attr("style", "color:orangered;font-size:10px;")
+					//.text(document.getElementById("currDate").textContent + ": File Layouts")
+					.html(function() {
+							return "<span style='color:grey;font-size:10px;'>File Layouts </span><span style='color:steelblue;font-size:11px;font-weight:bold;'>" + document.getElementById("currDate").textContent + "</span>";
+					})
+					//.attr("style", "color:orangered;font-size:10px;")
 					.attr("valign", "bottom")
 					.attr("align", "center");
 					
@@ -242,7 +261,10 @@ d3.select(".DailyData")
 		.attr("border", borderT)
 			.append("tr")
 				.append("td")
-					.text(document.getElementById("currDate").textContent + ": File Category")
+					.html(function() {
+							return "<span style='color:grey;font-size:10px;'>File Category </span><span style='color:steelblue;font-size:11px;font-weight:bold;'>" + document.getElementById("currDate").textContent + "</span>";
+					})
+					//.text(document.getElementById("currDate").textContent + ": File Category")
 					.attr("style", "color:orangered;font-size:10px;")
 					.attr("valign", "bottom")
 					.attr("align", "center");
@@ -258,9 +280,124 @@ var data = d3.entries(BarDaily_filterCatGroup);
 modularBarCharts (data, ".DailyDataDiv" );
 
 if(select_1 == "err" || select_1 == "email" || select_1 == "fileT") barErrSelectedData ();
+else showDataTable();
 
 //DailyDataFileTD
 }
+////////// SHOW DATA TABLE /////////////////////////////
+function showDataTable(){
+
+d3.select(".errDataDivT").remove();
+d3.select(".errDataDivTD").remove();
+d3.select(".errDataDiv").remove();
+d3.selectAll(".errDataDivC").remove();
+d3.select(".errDataDailyROW").remove();
+d3.select(".errDataDaily").remove();
+d3.selectAll("svg")
+			.select(".errDataDailyC").remove();
+			
+var data = [];
+var dataTemp = [];
+if(SelectedData != "") 
+{
+ //if(parseInt(SelectedData[0].File_Layouts) != parseInt(SelectedData[0].emails.length)) {
+ SelectedData[0].emails.forEach(function(d,i) {
+								/*var datarow = {"SlNo.":i+1,
+												"File": d.TimeStamp.substring(0,2) + ":" + d.TimeStamp.substring(2,4) + ":" + d.TimeStamp.substring(4,6) + " "  + d.File_Layouts ,
+												"Total": parseInt(d.Tot_Records),
+												"Inserts": parseInt(d.Tot_Inserts),
+												"Updates": parseInt(d.Tot_Updates),
+												"Failures": parseInt(d.Tot_Failures),
+												"Duration(Hrs)": Number(d.Tot_Duration_Hrs),
+												"Errors": parseInt(d.Err_Cnt)
+								};*/
+								var dataVal = getData(select_1,parseInt(d.Tot_Records),parseInt(d.Tot_Failures),parseInt(d.Tot_Inserts),parseInt(d.Tot_Updates),Number(d.Tot_Duration_Hrs),1,parseInt(d.Error_Msgs),parseInt(d.tot_emails));
+								var datarow = {"Sl.":i+1,
+												"Time": d.TimeStamp.substring(0,2) + ":" + d.TimeStamp.substring(2,4) + ":" + d.TimeStamp.substring(4,6),
+												"File": d.File_Layouts,
+												"Value": dataVal};
+								dataTemp.push(datarow);
+	
+});
+
+dataTemp.sort(function(a, b){ return d3.ascending(a.Time, b.Time); });
+dataTemp.forEach (function(d,i) {
+	var datarow = {"Sl.":i+1,
+					"Time": d.Time,
+					"File": d.File,
+					"Value": d.Value};
+	data.push(datarow);
+});
+
+//var columns = ["SlNo.", "File", "Total", "Inserts", "Updates", "Failures", "Duration(Hrs)","Errors"];
+///// DRAW DATA TABLE SECTION /////// START
+
+d3.selectAll(".dataTableSumm")
+	.append("td")
+    .attr("class", "errDataDivTD")
+	.attr("valign", "bottom")
+	.attr("style", "overflow:auto;")
+	.attr("align", "left");
+
+	var tblName = 	"dataTable";	
+d3.selectAll(".downloadBtn").remove();				
+d3.select(".errDataDivTD")
+	.attr("valign", "bottom")
+	.attr("align", "left")
+		.append("table")
+		//.attr("id", "DATATABLE")
+		.attr("id", tblName)
+		.attr("class", "errDataDivT")
+		.attr("height" ,"auto")
+		.attr("border", borderT)
+			.append("tr")
+				.append("td")
+				.attr("valign", "bottom")
+				.text("DATA LOAD TIME SEQUENCE")
+					.attr("style", "color:grey;font-size:10px;font-weight:bold;")
+					.attr("valign", "bottom")
+					.attr("align", "center");	
+
+					
+d3.select(".errDataDivT")
+	.append("tr")
+		.append("td")			
+		.text(document.getElementById("currDate").textContent +" : "+document.getElementById(select_1).textContent)
+					.attr("style", "color:steelblue;font-size:9px;font-weight:none;")
+					.attr("valign", "bottom")
+					.attr("align", "center");
+
+//var tblName = 	"dataTable";				
+d3.selectAll(".errDataDivT")
+	.append("tr")
+	.attr("class", "downloadBtn")
+		.append("td")
+		.attr("cellpadding", 4)
+				.append("input")
+					.attr("type", "button")
+					.attr("class", "button")
+					.attr("value", "Export")
+					//.on("click", function (){ tableToExcel("DATATABLE")}) 
+					.on("click", function (){ tableToExcel(tblName)})
+					
+d3.select(".errDataDivT")
+	.append("tr")
+		.append("td")
+			.attr("height" ,50)
+			.attr("class", "errDataDiv")
+			.attr("valign", "top")
+			.attr("align", "right");
+					
+var columns = ["Sl.", "Time", "File", "Value"];
+var columnStyles = ["font-size:9px;color:gray;", "font-size:9px;color:steelblue;", "font-size:9px;color:gray;", "font-size:9px;color:steelblue;"];
+var columnFormat = ["", "", "", "1"];
+modularDataTable(columns, data, columnStyles, ".errDataDiv", tblName, columnFormat);
+///// DRAW DATA TABLE SECTION /////// END
+			
+//}//if(parseInt(SelectedData[0].File_Layouts) != parseInt(SelectedData[0].emails.length)) {
+}//if(SelectedData != "") 
+}
+
 ///////// BUILD DATA FOR SELECTED DAY ERROR INFO ///////
 function barErrSelectedData (){
 var Bar_Error = [];
@@ -308,233 +445,5 @@ if(Bar_Error != "" ) {
 	modularBarCharts (data, ".errDataDaily" );
 	}//if(Bar_Error.length > 0 ) {	
 }
-///////////////	BUILD MODULAR BAR CHARTS
-function modularBarCharts (data, strC) {
+/////////////
 
-
-var valueLabelWidth = 40; // space reserved for value labels (right)
-
-var margin = {top: 5, right: 0, bottom: 5, left: 0},
-    width = (widthBarChart - margin.left - margin.right + valueLabelWidth) *3 + 200,
-    height = (10 * d3.entries(Bar_filterCatGroup).length) - margin.top - margin.bottom;
-
-// DRAWING BAR CHART STARTS - CATEGORY WISE
-
-//var valueLabelWidth = 40; // space reserved for value labels (right)
-var barHeight = 10; // height of one bar
-
-// space reserved for bar labels
-if (strC == ".SummDataFile" || strC == ".DailyDataFile") var barLabelWidth = 110; 
-else 
-{	if(strC == ".errDataDiv" || strC == ".errDataDaily") var barLabelWidth = 150; 
-	else var barLabelWidth = 60;
-}
-
-var barLabelPadding = 5; // padding between bar and bar labels (left)
-var gridLabelHeight = 0; // space reserved for gridline labels
-var gridChartOffset = 3; // space between start of grid and first bar
-
-/*if(strC == ".SummDataFile" || strC == ".SummDataDiv") var maxBarWidth = height +25 ; // width of the bar with the max value
-else var maxBarWidth = height +30 ; // width of the bar with the max value*/
-
-var maxBarWidth = height +30 ; // width of the bar with the max value
-
-//var maxBarWidth1 = maxBarWidth * -1
-var bottomPadding = 5; // padding at bottom of chart
-var gapBars = 2; // padding at bottom of chart
-
-// accessor functions 
-var barLabel = function(d) { return d.key; };
-var barValue = function(d) { return parseFloat(d.value); };
- 
-// scales
-var yScale = d3.scale.ordinal().domain(data.map(function(d) { return d.key; })).rangeRoundBands([0, data.length * barHeight], .2);
-var y = function(d, i) { return yScale(i); };
-var yText = function(d, i) { return y(d, i) + yScale.rangeBand() / 2; };
-var x = d3.scale.linear().domain(d3.extent(data, function(d) { return d.value; })).range([0, maxBarWidth]);
-
-
-// custom scal down
-var scaleDown = .8;
-var chart = d3.selectAll(strC).append("svg")
-  .attr('width', maxBarWidth + barLabelWidth + valueLabelWidth)
-  .attr('height', bottomPadding + gridLabelHeight + gridChartOffset + data.length * barHeight)
-  .attr("class" , strC.replace(".", "") + "C");
-  
-  // grid line labels
-/*var gridContainer = chart.append('g')
-  .attr('transform', 'translate(' + barLabelWidth + ',' + gridLabelHeight + ')'); 
-gridContainer.selectAll("text").data(x.ticks(10)).enter().append("text")
-  .attr("x", x)
-  .attr("dy", -3)
-  .attr("text-anchor", "middle")
-  .text(String);*/
-  
-// vertical grid lines
-/*gridContainer.selectAll("line").data(x.ticks(10)).enter().append("line")
-  .attr("x1", x)
-  .attr("x2", x)
-  .attr("y1", 0)
-  .attr("y2", yScale.rangeExtent()[1] + gridChartOffset)
-  .style("stroke", "#ccc");*/
-
-// bar labels
-var labelsContainer = chart.append('g')
-  .attr('transform', 'translate(' + (barLabelWidth - barLabelPadding) + ',' + (gridLabelHeight + gridChartOffset) + ')');  
-labelsContainer.selectAll('text').data(data).enter().append('text')
-  .attr('y', yText)
-  //.attr('x', barLabelWidth)
-  .attr('stroke', 'none')
-  .attr('class', function (d) { var Label = "";
-						if (strC == ".SummDataFile" || strC == ".DailyDataFile" || strC == ".errDataDiv" || strC == ".errDataDaily") 
-							{
-								Label = d.key; }
-							else 
-							{ 
-								//console.log("getCatDesc(d.key)");
-								//console.log(getCatDesc(d.key));
-								Label =  getCatDesc(d.key) }
-						//console.log(Label);
-						return Label;
-					})
-	.attr('id', function (d) { var Label = "";
-						if (strC == ".SummDataFile" || strC == ".DailyDataFile" || strC == ".errDataDiv" || strC == ".errDataDaily") 
-							{
-								Label = d.key; }
-							else 
-							{ 
-								//console.log("getCatDesc(d.key)");
-								//console.log(getCatDesc(d.key));
-								Label =  getCatDesc(d.key) }
-						//console.log(Label);
-						return Label;
-					})
-  //.attr('fill', 'steelblue')
-  .attr('style', function(d) {
-			if (d.value > 0) {
-				if (strC == ".DailyDataFile" || strC == ".DailyDataDiv" || strC == ".errDataDaily") { 
-					//console.log(document.getElementById(this.id).className.baseVal); 
-					//console.log(document.getElementById(this.ClassName));
-					/*if(strC != ".SummDataFileC") {
-					var strClass = document.getElementById(this.id).className.baseVal;
-					//var others = d3.selectAll(strClass);
-					var objSummLabels = d3.select(".SummDataFileC").selectAll("g")[0];
-					var objSummLabels_g = objSummLabels[0].childNodes;
-					//console.log(objSummLabels_g.length);
-					for(var i =0; i < objSummLabels_g.length; i++ ) {
-						if(objSummLabels_g[i].className.baseVal == strClass ) {
-							//console.log("x.className.baseVal");
-							console.log(objSummLabels_g[i].style.fill);
-							objSummLabels_g[i].style.fill == 'orangered';
-						}
-					};
-					//var objCalendar_g_children = objCalendar_g[0].children;
-					}//if(strC != ".SummDataFileC")
-					//document.getElementById(this.id).style.fill = 'orangered'; */
-					return 'fill:orangered';
-					
-					} //if (strC == ".DailyDataFile" || strC == ".DailyDataDiv" || strC == ".errDataDaily")
-				else { return 'fill:steelblue'} 
-			}
-			else return 'fill:grey';
-			})
-  .attr('font-weight', function(d) {if (d.value > 0) { return 'bold' } else {return '';}} )
-  .attr("dy", ".35em") // vertical-align: middle
-  //.attr('text-anchor', 'middle')
-  .attr('text-anchor', 'end')
-  .text(function (d) { var Label = "";
-						if (strC == ".SummDataFile" || strC == ".DailyDataFile" || strC == ".errDataDiv" || strC == ".errDataDaily") 
-							{
-								Label = d.key; }
-							else 
-							{ 
-								//console.log("getCatDesc(d.key)");
-								//console.log(getCatDesc(d.key));
-								Label =  getCatDesc(d.key) }
-						//console.log(Label);
-						return Label;
-					}); 
- // } // if (strC != ".SummDataFile")
-// bars
-var barsContainer = chart.append('g')
- .attr('transform', 'translate(' + barLabelWidth + ',' + (gridLabelHeight + gridChartOffset) + ')'); 
- //.attr('transform', 'translate(' + barLabelWidth + ',' + (gridLabelHeight + gridChartOffset) + '), rotate(-180)');
- //.attr('transform', 'translate(' + barLabelWidth + ', 180), rotate(-180)'); 
-  
-var rect = barsContainer.selectAll("rect").data(data).enter().append("rect")
-  
-  //.attr('y', y)
-  .attr('y', function(d) { return yScale(d.key); })
- /* .attr("x", function(d) { 
-		if (strC == ".SummDataFile" || strC == ".SummDataDiv") return x(Math.min(0, -d.value)) * scaleDown +10; 
-		else return (x(Math.min(0, d.value)) * scaleDown)+10;  
-		
-		})*/
-  .attr('height', yScale.rangeBand())
-  
-  .attr('width', function(d) { return x(barValue(d))* scaleDown; })
-  //.attr("width", function(d) { if (strC == ".SummDataFile" || strC == ".SummDataDiv") return Math.abs(x(-d.value) - x(0)) * scaleDown; else return Math.abs(x(d.value) - x(0))* scaleDown;})
-  
-  .attr('stroke', 'white')
-  //Chartreuse
-  .attr('fill', function () { if(strC == ".DailyDataFile" || strC == ".DailyDataDiv" || strC == ".errDataDaily") {return 'steelblue'} else {return 'steelblue'};})
-  .on('mouseover', function(){ this.style.fill = 'red'; return;} )
- .on('mouseout', function(){if(strC == ".DailyDataFile" || strC == ".DailyDataDiv" || strC == ".errDataDaily") {this.style.fill = 'steelblue'} else {this.style.fill = 'steelblue'};});
- 
-// bar value labels
-barsContainer.selectAll("text").data(data).enter().append("text")
-  .attr("x", function(d) { return  x(barValue(d)) * scaleDown; })
-  /*.attr("x", function(d) { 
-		if (strC == ".SummDataFile" || strC == ".SummDataDiv") return -1 * x(barValue(d)) * scaleDown+10; 
-		else return  x(barValue(d)) * scaleDown +10; 
-		})*/
-  //.attr("x", function(d) { if (strC == ".SummDataFile") return x(Math.min(0, -d.value)) * scaleDown; else return x(Math.min(0, d.value)) * scaleDown;  })
-  
-  .attr("y", yText)
-  .attr("dx", 3) // padding-left
-  /*.attr("dx", function(d) { 
-	if (strC == ".SummDataFile" || strC == ".SummDataDiv") return -3 ; 
-	
-	else return 3;} ) // padding-left*/
-  
-  .attr("dy", ".35em") // vertical-align: middle
-  
-  .attr("text-anchor", "start") // text-align: right
-  //.attr("text-anchor", function(d) { if (strC == ".SummDataFile" || strC == ".SummDataDiv") return "end" ; else return "start";} )
-  .attr("fill", 'steelblue')
-  .attr("stroke", "none")
-  .attr("font-size", "9px")
-  .attr("align", "left")
-  .text(function(d) { if (barValue(d) > 0 ) return addCommas(d3.round(barValue(d), 2)); });
-  
-  
-// start lin
-barsContainer.append("line")
-  .attr("y1", -gridChartOffset)
-  .attr("y2", yScale.rangeExtent()[1] + gridChartOffset)
-  /*.attr("x1", function(d) { 
-		if (strC == ".SummDataFile" || strC == ".SummDataDiv") return 10; 
-		else return  10; 
-		})
-  .attr("x2", function(d) { 
-		if (strC == ".SummDataFile" || strC == ".SummDataDiv") return 10; 
-		else return  10; 
-		})*/
-  .style("stroke", "#000");
-
-if (strC == ".SummDataFile" || strC == ".DailyDataFile"  || strC == ".SummDataDiv" || strC == ".errDataDaily") 
-	{
-		rect.append('title')
-			.text(function(d) {return d.key + ": " + addCommas(d.value);});
-	}
-else
-	{
-		rect.append('title')
-			.text(function(d) {return document.getElementById(select_1).textContent + " - " + getCatDesc(d.key) + ": " + addCommas(d.value);});
-	}
-	 
-return;
-	 
-// DRAWING BAR CHART STARTS - CATEGORY WISE
-}
-/////////
